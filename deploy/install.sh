457e14508c399b759e07a849b20647b6af8ba54b
Jordan Tart <jordan.tart@bbc.co.uk>
2018-02-14 21:50:08 +0000

    force symbolic link

--- deploy/install.sh
+++ deploy/install.sh
@@ -7,7 +7,7 @@ apt-get install python-software-properties -y
 curl -sL https://deb.nodesource.com/setup_9.x | sudo -E bash -
 apt-get install nodejs -y
 
-ln -s /usr/bin/nodejs /usr/bin/node
+ln -sf /usr/bin/nodejs /usr/bin/node
 
 npm install -g --no-optional pm2
 pm2 update

3861aab21210e77ede08997e034e1206d03e02ca
Jordan Tart <jordan.tart@bbc.co.uk>
2018-02-14 19:32:37 +0000

    remove unneeded deps; use pm2

--- next.config.js
+++ next.config.js
@@ -1,16 +1,9 @@
 const webpack = require('webpack');
-const { WebpackBundleSizeAnalyzerPlugin } = require('webpack-bundle-size-analyzer')
 
 module.exports = {
   webpack: (config, {dev}) => {
     if (dev) {
       return config
-    }
-
-    if (process.env.npm_config_report) {
-      config.plugins.push(
-        new WebpackBundleSizeAnalyzerPlugin('stats.txt')
-      )
     }
 
     config.plugins = config.plugins.filter(

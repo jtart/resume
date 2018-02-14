34e3f419fea898a9bbebb8560172c8fce1af1ea4
Jordan Tart <jordan.tart@bbc.co.uk>
2018-02-14 20:21:22 +0000

    fix dir

--- deploy/index.js
+++ deploy/index.js
@@ -5,7 +5,7 @@ var NodeDeployment = require("codedeploy-scripts").NodeDeployment;
 var deployment = new NodeDeployment({
     appName: "jordan-tart",
     nodePort: "3000",
-    serverScript: "build/server/index.js"
+    serverScript: "server.js"
 });
 
 deployment.run();

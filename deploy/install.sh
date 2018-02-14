1a4b4628087aa2ebe3916f5f8e548fa8c08e2b0d
Jordan Tart <jordan.tart@bbc.co.uk>
2018-02-14 21:42:01 +0000

    explicit install node 9

--- deploy/install.sh
+++ deploy/install.sh
@@ -2,8 +2,10 @@
 set -e
 
 apt-get update -y
+
+apt-get install python-software-properties -y
+curl -sL https://deb.nodesource.com/setup_9.x | sudo -E bash -
 apt-get install nodejs -y
-apt-get install npm -y
 
 ln -s /usr/bin/nodejs /usr/bin/node
 

9b0de28434dd617b010d18f06a989241fe0650fe
Jordan Tart <jordan.tart@bbc.co.uk>
2018-02-15 13:22:36 +0000

    Add warning to stop script

--- deploy/app_stop.sh
+++ deploy/app_stop.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+### WARNING ###
+# Be careful when making changes to this script as you will have to do a manual deploy ignoring the app stop step if this script is broken.
+
 pm2 stop server
 
 rm -rf /home/ubuntu/jordan-tart

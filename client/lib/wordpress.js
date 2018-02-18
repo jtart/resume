8e8ff804129090b32dc6735ab7aa4f46c7623ded
Jordan Tart <jordan.tart@bbc.co.uk>
2018-02-18 20:56:43 +0000

    Use dotenv for config

--- client/lib/wordpress.js
+++ client/lib/wordpress.js
@@ -1,5 +1,6 @@
 import fetch from "isomorphic-unfetch"
-import { Config } from "./config.js"
+
+const apiURL = process.env.APIURL
 
 const fetchJson = async (url, options = null) => {
   const res = await fetch(url, options)
@@ -7,28 +8,28 @@ const fetchJson = async (url, options = null) => {
 }
 
 const getCategory = async ({ slug }) => {
-  const categoriesUrl = `${Config.apiUrl}/wp-json/wp/v2/categories?slug=${slug}`
+  const categoriesUrl = `${apiURL}/wp-json/wp/v2/categories?slug=${slug}`
   const categories = await fetchJson(categoriesUrl);
 
   if (categories.length <= 0) {
     return { categories }
   }
 
-  const categoryPostsUrl = `${Config.apiUrl}/wp-json/wp/v2/posts?_embed&categories=${categories[0].id}`
+  const categoryPostsUrl = `${apiURL}/wp-json/wp/v2/posts?_embed&categories=${categories[0].id}`
   const posts = await fetchJson(categoryPostsUrl)
 
   return { categories, posts }
 }
 
 const getPage = async ({ slug = 'welcome', apiRoute = 'page' }) => {
-  const url = `${Config.apiUrl}/wp-json/postlight/v1/${apiRoute}?slug=${slug}`
+  const url = `${apiURL}/wp-json/postlight/v1/${apiRoute}?slug=${slug}`
   const data = await fetchJson(url)
 
   return { data }
 }
 
 const getPreview = async (id, wpnonce) => {
-  const url = `${Config.apiUrl}/wp-json/postlight/v1/post/preview?id=${id}&_wpnonce=${wpnonce}`
+  const url = `${apiURL}/wp-json/postlight/v1/post/preview?id=${id}&_wpnonce=${wpnonce}`
   const options = { credentials: "include" }
   const data = await fetchJson(url, options)
 
@@ -36,7 +37,7 @@ const getPreview = async (id, wpnonce) => {
 }
 
 const getMenu = async () => {
-  const url = `${Config.apiUrl}/wp-json/menus/v1/menus/header-menu`
+  const url = `${apiURL}/wp-json/menus/v1/menus/header-menu`
   const data = await fetchJson(url)
 
   return data
--- client/next.config.js
+++ client/next.config.js
@@ -1,7 +1,12 @@
 const webpack = require('webpack');
+require('dotenv').config()
 
 module.exports = {
   webpack: (config, {dev}) => {
+    config.plugins.push(
+      new webpack.EnvironmentPlugin(process.env)
+    )
+
     if (dev) {
       return config
     }

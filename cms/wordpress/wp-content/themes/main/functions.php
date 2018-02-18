5e57725cbd4555b7503bcd04e12d62ad3fcfa0a8
Jordan Tart <jordan.tart@bbc.co.uk>
2018-02-18 20:30:27 +0000

    Add wordpress backend

---
+++ cms/wordpress/wp-content/themes/main/functions.php
@@ -0,0 +1,19 @@
+<?php
+
+// Frontend origin
+require_once( 'inc/frontend-origin.php' );
+
+// Logging functions
+require_once( 'inc/log.php' );
+
+// CORS handling
+require_once( 'inc/cors.php' );
+
+// Admin modifications
+require_once( 'inc/admin.php' );
+
+// Add Menus
+require_once( 'inc/menus.php' );
+
+// Add custom API endpoints
+require_once( 'inc/api-routes.php' );
